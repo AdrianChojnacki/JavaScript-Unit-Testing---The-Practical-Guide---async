d94ddfe16682fb772195bded1b192bad7156aa10
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-05-31 16:25:05 +0200

    added working try-catch test

--- async/async-example.test.js
+++ async/async-example.test.js
@@ -1,11 +1,18 @@
 import { expect, it } from "vitest";
 import { generateToken } from "./async-example";
 
-it("should generate a token value", () => {
+it("should generate a token value", done => {
   const testUserEmail = "test@test.pl";
 
   generateToken(testUserEmail, (err, token) => {
     // expect(token).toBeDefined();
-    expect(token).toBe(2);
+
+    try {
+      expect(token).toBeDefined();
+      // expect(token).toBe(2);
+      done();
+    } catch (err) {
+      done(err);
+    }
   });
 });
